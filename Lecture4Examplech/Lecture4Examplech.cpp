1c2099a6890bcdb6e8522bd166d3aaef19abad91
Chewlett <hewlettcd@vcu.edu>
2017-02-06 21:19:09 -0500

    Add project files.

---
+++ Lecture4Examplech/Lecture4Examplech.cpp
@@ -0,0 +1,21 @@
+// Lecture4Examplech.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	int i = 10;
+	int *ptr1 = &i;
+
+	cout << i << endl;
+	cout << ptr1 << endl;
+	cout << &i << endl;
+	cout << &ptr1 << endl;
+
+	return 0;
+}
+
